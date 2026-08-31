81d573a9b4f3bbbbc673adfa4b249cf556f14e69
Alex <249374@rambler.ru>
2026-08-31 10:38:08 +0600

    ContextVarBoundLogger is no longer used in structlog

--- app/core/logging.py
+++ app/core/logging.py
@@ -14,7 +14,6 @@ def setup_logging():
             structlog.processors.JSONRenderer()
         ],
         wrapper_class=structlog.make_filtering_bound_logger(logging.getLevelName(settings.log_level.upper())),
-        context_class=structlog.contextvars.ContextVarBoundLogger,
         logger_factory=structlog.PrintLoggerFactory(),
         cache_logger_on_first_use=True,
     )
